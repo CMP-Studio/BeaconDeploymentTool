c4db1a004afc7023b58f90296c32ef2b0d73d43c
Ruben NIculcea <ruben.niculcea@gmail.com>
2017-03-21 14:02:18 -0400

    use prettier-eslint for git hook

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { Provider } from "react-redux";
+import React from 'react';
+import { Provider } from 'react-redux';
 
-import configureStore from "./store";
+import configureStore from './store';
 
-import RootScreen from "./containers/rootScreen";
+import RootScreen from './containers/rootScreen';
 
 const store = configureStore();
 
