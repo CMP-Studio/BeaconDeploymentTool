5403a937787b1b00e72a05805891d7dbe2121b7a
Ruben NIculcea <ruben.niculcea@gmail.com>
2017-07-10 09:12:46 -0400

    Display an alert if block rules file is not proper JSON

--- src/actions/data.js
+++ src/actions/data.js
@@ -1,3 +1,4 @@
+import { Alert } from 'react-native';
 import fs from 'react-native-fs';
 
 import { beaconJSONToBeaconMap } from '../utilities';
@@ -29,18 +30,30 @@ export function saveBeaconsToFile() {
 
 export function loadBeaconsFromFile() {
   return async (dispatch, getState) => {
+    if (__DEV__) {
+      console.log(fs.DocumentDirectoryPath);
+    }
+
     const filePath = `${fs.DocumentDirectoryPath}/${FILE_NAME}`;
 
     try {
-      const beaconsJSON = await fs.readFile(filePath, 'utf8');
-      const allBeacons = beaconJSONToBeaconMap(beaconsJSON);
+      const exists = await fs.exists(filePath);
 
-      return dispatch({
-        type: LOAD_BEACONS_FROM_FILE,
-        allBeacons,
-      });
+      if (exists) {
+        const beaconsJSON = await fs.readFile(filePath, 'utf8');
+        const allBeacons = beaconJSONToBeaconMap(beaconsJSON);
+
+        return dispatch({
+          type: LOAD_BEACONS_FROM_FILE,
+          allBeacons,
+        });
+      }
     } catch (e) {
-      console.log(e);
+      // Not happy with this but it's okay for now.
+      Alert.alert(
+        'Beacon JSON',
+        'The beaconBlockRules.json file is not valid JSON. Please check the file with an online JSON validator to help find and fix the error and reload the file.',
+      );
     }
   };
 }
